daf5f2bf3eafdb21c6ecfb29198c924899d3d443
Raymond Feckoury <rjf21434@uga.edu>
2022-07-07 20:41:39 +0000

    Add servlets for Questions, Meet Ups, and Replys. Create a question class that can store responses.

--- src/main/java/com/google/sps/servlets/NewMeetUp.java
+++ src/main/java/com/google/sps/servlets/NewMeetUp.java
@@ -11,24 +11,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet responsible for creating new queries. */
-@WebServlet("/new-query")
-public class Query extends HttpServlet {
+/** Servlet responsible for creating new questions. */
+@WebServlet("/new-meetup")
+public class NewMeetUp extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String text = request.getParameter("question");
+    String text = request.getParameter("meet-up");
     long timestamp = System.currentTimeMillis();
 
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Query");
-    FullEntity taskEntity =
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("MeetUp");
+    FullEntity entity =
         Entity.newBuilder(keyFactory.newKey())
             .set("text", text)
             .set("time", timestamp)
             .build();
-    datastore.put(taskEntity);
+    datastore.put(entity);
 
     response.sendRedirect("https://summer22-sps-52.appspot.com");
   }
 }
+
